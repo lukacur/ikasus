9b3213f3f6d3e2f1a3ec64fa72a8f9e9d75de92c
lukacur <92858303+lukacur@users.noreply.github.com>
2023-05-16 17:06:54 +0200

    Added contract controller

---
+++ src/main/java/hr/fer/ika/ikasus/controller/ContractController.java
@@ -0,0 +1,75 @@
+package hr.fer.ika.ikasus.controller;
+
+import hr.fer.ika.ikasus.DTO.incoming.UpdateContractDetail;
+import hr.fer.ika.ikasus.DTO.outgoing.ContractDetail;
+import hr.fer.ika.ikasus.DTO.outgoing.ContractMaster;
+import hr.fer.ika.ikasus.service.ContractService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+/**
+ * @author Luka Ćurić
+ */
+@RestController
+@RequestMapping(
+        value = "/api/authenticated/contracts",
+        produces = { MediaType.APPLICATION_JSON_VALUE }
+)
+public class ContractController {
+    private final ContractService contractService;
+
+    public ContractController(ContractService contractService) {
+        this.contractService = contractService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ContractMaster>> getContracts() {
+        return ResponseEntity.ok(this.contractService.getContractMasters());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContractDetail> getContractDetail(@PathVariable("id") Integer contractId) {
+        ContractDetail detail = this.contractService.getDetailsFor(contractId);
+
+        if (detail == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(detail);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createContract(
+            HttpServletRequest req,
+            @RequestBody UpdateContractDetail contractDetail
+    ) {
+        Integer id = this.contractService.createContract(contractDetail);
+
+        if (id == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        String createdURL = req.getServletPath() + "/" + id;
+
+        return ResponseEntity.created(URI.create(createdURL)).build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> updateContractDetails(
+            @PathVariable("id") Integer contractId,
+            @RequestBody UpdateContractDetail contractDetailUpdate
+    ) {
+        boolean success = this.contractService.updateDetailsFor(contractId, contractDetailUpdate);
+
+        if (!success) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+}
