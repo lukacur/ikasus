e9b8a3c519580297411487ab027e0f5115a74344
lukacur <92858303+lukacur@users.noreply.github.com>
2023-05-16 12:09:16 +0200

    Updated repositories for authentication purposes

--- src/main/java/hr/fer/ika/ikasus/repository/UnajmiteljRepository.java
+++ src/main/java/hr/fer/ika/ikasus/repository/UnajmiteljRepository.java
@@ -3,5 +3,8 @@ package hr.fer.ika.ikasus.repository;
 import hr.fer.ika.ikasus.DAO.Unajmitelj;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UnajmiteljRepository extends JpaRepository<Unajmitelj, Integer> {
+    Optional<Unajmitelj> findByEmail(String email);
 }
--- src/main/java/hr/fer/ika/ikasus/repository/UpraviteljPoslovniceRepository.java
+++ src/main/java/hr/fer/ika/ikasus/repository/UpraviteljPoslovniceRepository.java
@@ -3,5 +3,8 @@ package hr.fer.ika.ikasus.repository;
 import hr.fer.ika.ikasus.DAO.UpraviteljPoslovnice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UpraviteljPoslovniceRepository extends JpaRepository<UpraviteljPoslovnice, Integer> {
+    Optional<UpraviteljPoslovnice> findByKorisnickoime(String korisnickoime);
 }
