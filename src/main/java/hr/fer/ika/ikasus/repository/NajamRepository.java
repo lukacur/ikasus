7f6683d1cd9deeb2c73de0a00bb57134aedfe99d
lukacur <92858303+lukacur@users.noreply.github.com>
2023-05-17 00:57:31 +0200

    Updated najam repository with fetch by vehicle id method

--- src/main/java/hr/fer/ika/ikasus/repository/NajamRepository.java
+++ src/main/java/hr/fer/ika/ikasus/repository/NajamRepository.java
@@ -1,7 +1,11 @@
 package hr.fer.ika.ikasus.repository;
 
 import hr.fer.ika.ikasus.DAO.Najam;
+import hr.fer.ika.ikasus.DAO.Vozilo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NajamRepository extends JpaRepository<Najam, Integer> {
+    List<Najam> findByIdvozilo(Vozilo vozilo);
 }
