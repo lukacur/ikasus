1b21f4b16e90db3d4b2f637f65a6050087aedbdc
lukacur <92858303+lukacur@users.noreply.github.com>
2023-05-17 15:24:00 +0200

    Removed spring security exclusion rule

--- src/main/java/hr/fer/ika/ikasus/IkasusApplication.java
+++ src/main/java/hr/fer/ika/ikasus/IkasusApplication.java
@@ -2,9 +2,8 @@ package hr.fer.ika.ikasus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication(exclude = { org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class IkasusApplication {
 
 	public static void main(String[] args) {
